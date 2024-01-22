15426101cb9d9aede5bc32aa19387e4da7eb25c3
Ege Aydemir <egeaydemirr97@gmail.com>
2024-01-22 21:46:44 +0300

    Save Button and some changes added

--- SwiftExercises/Controllers/SegueFirstPageViewController.swift
+++ SwiftExercises/Controllers/SegueFirstPageViewController.swift
@@ -8,25 +8,29 @@
 import UIKit
 
 class SegueFirstPageController: UIViewController {
+    @IBOutlet weak var lbl1Segue: UILabel!
+    @IBOutlet weak var btnSave: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Second Screen"
-        navigationItem.title = "Segue First Page"
-        navigationItem.prompt = "Prompt 3"
         config()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var lbl1Segue: UILabel!
-    
     func config() {
-        lbl1Segue.text = "Segue 1 Label"
-        lbl1Segue.textColor = UIColor.white
-        lbl1Segue.font = UIFont.systemFont(ofSize: 36)
+        title = "Second Screen"
+        navigationItem.title = "Segue First Page"
+        navigationItem.prompt = "Prompt 3"
+        lbl1Segue.text = "Adinizi Giriniz"
+        lbl1Segue.textColor = UIColor.black
+        lbl1Segue.font = UIFont.boldSystemFont(ofSize: 18)
+        btnSave.setTitle("Kaydet", for: .normal)
     }
 
+    @IBAction func saveBtnClicked(_ sender: Any) {
+        print("Save Button Clicked")
+    }
     /*
     // MARK: - Navigation
 
